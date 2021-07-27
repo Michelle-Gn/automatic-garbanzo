ab0b76d8ce5e4e4b345d0791a3098dccdcf62eb0
Matthew Kwon <matthew.o.kwon@gmail.com>
2021-07-27 15:01:25 -0700

    Implemented basic top level component

--- client/src/components/ReviewsRatings/ReviewsRatings.jsx
+++ client/src/components/ReviewsRatings/ReviewsRatings.jsx
@@ -1,16 +1,20 @@
-// import React
+import React from 'react';
 // import Review List contaner
 // import helpers
 
-const ReviewsRatings = () => {
+const ReviewsRatings = (props) => {
   // useState for current product ID
 
   // get ratings data with current product ID
   // useEffect for metaData
 
-  // return
-    // Breakdown
-    // ReviewList
+  return (
+    <div>
+      <h1>Ratings &amp; Reviews</h1>
+      <Breakdown />
+      <ReviewList />
+    </div>
+  )
 };
 
 export default ReviewsRatings;
