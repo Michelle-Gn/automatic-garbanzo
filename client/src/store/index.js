32bef01cd4408d9d611cad56f0ce13786c4337b5
Matthew Kwon <matthew.o.kwon@gmail.com>
2021-07-27 17:33:23 -0700

    Moved around directories and changed file paths to try and not break anything

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-
-// https://codeburst.io/redux-devtools-for-dummies-74566c597d7
 import {composeWithDevTools} from 'redux-devtools-extension';
-
 import rootReducer from '../components/reducers/';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
