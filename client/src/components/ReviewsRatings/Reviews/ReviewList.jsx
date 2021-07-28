17d34d5786ba146ad4a58845469f726c8ed38140
Matthew Kwon <matthew.o.kwon@gmail.com>
2021-07-27 17:00:43 -0700

    Implemented ReviewList component partially

--- client/src/components/ReviewsRatings/Reviews/ReviewList.jsx
+++ client/src/components/ReviewsRatings/Reviews/ReviewList.jsx
@@ -1,15 +1,20 @@
-// import React
-// import review component
-// import write review form component
-// import helpers
+import React, {useEffect, useState} from 'react';
+import Review from './Review.jsx';
+// import {getReviews} from '../../../../../helpers/getReviews.js';
 
-const ReviewList = () => {
+const ReviewList = (props) => {
   // declare current product ID
+  const productId = props.currentProduct.id;
   // useState for modal (writing new reviews)
   // useState for sort
+  const [sortBy, setSortBy] = useState('relevant');
 
   // if product ID changes
   // useEffect for getting reviews (with helper)
+  useEffect(() => {
+    const count = 5 // change to totalRatings from reducer eventually
+    getReviews(productId, sortBy, count, props.addReviews);
+  }, [productId, sortBy]);
 
   // handle "more reviews" click
     // ????
@@ -24,6 +29,20 @@ const ReviewList = () => {
       // option newest
     // add review button (write review form component)
     // review component (map)
+    return (
+      <div id='review-list'>
+        <div id='review-dropdown'>sorted by
+          <select onChange={(e) => setSortBy(e.target.value)}>
+            <option key='relevant' value='relevant'>Relevant</option>
+            <option key='helpful' value='helpful'>Helpful</option>
+            <option key='newest' value='newest'>Newest</option>
+          </select>
+        </div>
+        <div id='inner-review-list'>
+          {/* add reviews by mapping */}
+        </div>
+      </div>
+    )
 };
 
 export default ReviewList;
