ca6f1d0336157175763754f81ab217a26e08ba05
Michelle-Gn <michelle.guan93@gmail.com>
2021-07-29 16:38:57 -0700

    add q&a action type

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,7 +1,6 @@
 // This file serves as a onestop shop to change the value of Action.types.
 // It is used by reducers and action creators.
 
-export const GET_NEW_PRODUCT = 'GET_NEW_PRODUCT';
 // Used in Product Overview Reducers
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const UPDATE_SELECTED_STYLE = 'UPDATE_SELECTED_STYLE';
@@ -9,3 +8,5 @@ export const UPDATE_STYLE_LIST = 'UPDATE_STYLE_LIST'
 // Used in Reviews Reducers
 export const GET_RATINGS = 'GET_RATINGS';
 export const SET_TOTAL_RATINGS_AND_AVERAGE = 'SET_TOTAL_RATINGS_AND_AVERAGE';
+// Used in Question Answer
+export const GET_QUESTIONS = 'GET_QUESTIONS';
