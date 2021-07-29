6ea2056a5cf137f31054b01dbcb5af59152b70f5
Matthew Kwon <matthew.o.kwon@gmail.com>
2021-07-29 10:35:10 -0700

    Added R&R reducers to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,20 @@
 import { combineReducers } from 'redux';
-import updateStyleReducer from './StyleSelector/styleReducer'
-import addToCartReducer from './AddtoCart/cartReducer'
-import reviewsRatingsReducers from './R&R/reviewsRatingsReducers'
-import getNewProductReducer from './ProductOverview/getNewProductReducer'
+import updateStyleReducer from './StyleSelector/styleReducer';
+import addToCartReducer from './AddtoCart/cartReducer';
+import getNewProductReducer from './ProductOverview/getNewProductReducer';
+
+import ratingsReducers from './R&R/ratingsReducers.js';
+import reviewListReducers from './R&R/reviewListReducers.js';
 
 export default combineReducers({
-
   getNewProductReducer: getNewProductReducer,
   // Product Overview Reducers
   addToCartReducer: addToCartReducer,
   updateStyleReducer: updateStyleReducer,
+  // Ratings and Review Reducers
+  reviews: reviewListReducers.reviewListReducer,
+  visibleReviews: reviewListReducers.addReviewsReducer,
+  ratingsMeta: ratingsReducers.ratingsReducer,
+  totalRatings: ratingsReducers.totalRatingsReducer,
+  starAverage: ratingsReducers.starAverageReducer,
 })
