99bdd2883e23f8c4862fb521d2dbb65a9c20460b
Matthew Kwon <matthew.o.kwon@gmail.com>
2021-07-29 09:54:12 -0700

    Implemented starAverageReducer

--- client/src/reducers/R&R/ratingsReducer.js
+++ client/src/reducers/R&R/ratingsReducer.js
@@ -20,7 +20,14 @@ const reviewsRatingsReducers = {
     }
   },
 
-  starAverageReducer: () => {}
+  starAverageReducer: (state = 0, action) => {
+    switch (action.type) {
+    case actionTypes.SET_TOTAL_RATINGS_AND_AVERAGE:
+      return action.starAverage;
+    default:
+      return state;
+    }
+  }
 };
 
 export default reviewsRatingsReducers;
