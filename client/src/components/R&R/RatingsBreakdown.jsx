9227f76bd5f78fe6d308267d4e7e621f64ef02ef
Matthew Kwon <matthew.o.kwon@gmail.com>
2021-07-29 15:34:16 -0700

    Connected state to StarRating component

--- client/src/components/R&R/RatingsBreakdown.jsx
+++ client/src/components/R&R/RatingsBreakdown.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import StarRating from './StarRating.jsx';
 import RatingBar from './RatingBar.jsx';
 import CharacteristicBar from './CharacteristicBar.jsx';
 
+
 const RatingsBreakdown = (props) => {
+  const starAverage = useSelector(state => state.starAverage.starAverage)
+
   return (
     <div id='breakdown'>
       <h1>:: ratings breakdown ::</h1>
       <span id='starAverage'>
-        <StarRating />
+        <StarRating starCount={starAverage}/>
       </span>
       <div id='rating-bars'>
         <RatingBar />
