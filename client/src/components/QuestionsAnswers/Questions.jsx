2934831280b583016f4f3a94be87b3c208e7fef5
Michelle-Gn <michelle.guan93@gmail.com>
2021-07-29 16:45:30 -0700

    use dispatch to get questions list

--- client/src/components/QuestionsAnswers/Questions.jsx
+++ client/src/components/QuestionsAnswers/Questions.jsx
@@ -1,63 +1,44 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import QuestionsEntry from './QuestionsEntry.jsx';
+import getQuestions from '../../actions/Q&A/getQuestions.js';
 
-	class Questions extends React.Component {
-	  constructor(props) {
-	    super(props);
-	    this.state = {
-	      product_id : 16056,
-	      allQuestions: [],
-	      allAnswers: [],
-	      currentQuestions: []
-	    };
-	    this.getQuestions = this.getQuestions.bind(this)
-	  }
+var Questions = (props) => {
+ // uses an action and dispatches it to the store to modify state
+ // the action is passed through all the reducers in the store and the relevant
+ // reducer uses the action to change state
 
-	  componentDidMount() {
-	    this.getQuestions();
-	    /* instead of this.getQuestions(), would do props.getQuestions()*/
-	    /* instead of setState, would use dispatch*/
-	  }
+	const dispatch = useDispatch();
+// defines dispatch
+  getQuestions(dispatch, '16058');
+// invokes the getQuestions action, with dispatch as arg1, and product_id as arg 2
+	const questions = useSelector(state => state.questions)
+// useSelector gets pieces of data from global state
 
-	  getQuestions() {
-	    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions?product_id=${this.state.product_id}`,
-	    { headers: {'Authorization': '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'}})
-	    .then((results) => {
-	      this.setState ({
-	        allQuestions: results.data.results
-	      })})
-	    .catch((error) => {
-	      console.log(error)
-	    });
-	  }
-
-
-	  getAnswer() {
-	    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions?product_id=${this.state.product_id}`,
-	    { headers: {'Authorization': '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'}})
-	    .then((results) => {
-	      this.setState ({
-	        allQuestions: results.data.results
-	      })})
-	    .catch((error) => {
-	      console.log(error)
-	    });
-	  }
-
-
-	  render() {
-	    return (
-	      <div>
-	        <ul>
-	          {this.state.allQuestions.map((question, index) => (
-	            <QuestionsEntry question = {question} key = {index}/>
-	          ))}
-	        </ul>
-	      </div>
-	    )
-	  }
-	}
+	return (
+		<div>
+			<ul>
+				{questions.map((question, index) => (
+					<QuestionsEntry question = {question} key = {index}/>
+				))}
+			</ul>
+		</div>
+	)
+}
 
 
 	export default Questions
+
+
+	// getAnswer() {
+	// 	axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions?product_id=${this.state.product_id}`,
+	// 	{ headers: {'Authorization': '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'}})
+	// 	.then((results) => {
+	// 		this.setState ({
+	// 			allQuestions: results.data.results
+	// 		})})
+	// 	.catch((error) => {
+	// 		console.log(error)
+	// 	});
+	// }
