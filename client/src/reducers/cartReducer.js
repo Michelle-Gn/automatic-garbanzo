9e23066fd170485a676d0c09de4de3439a2b2ea0
Michelle-Gn <michelle.guan93@gmail.com>
2021-07-29 09:24:42 -0700

    update cart reducer

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -5,12 +5,13 @@ const initialState = {
 }
 
 const addToCartReducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch(action.type) { // different way to write conditionals, argument is action type
     case actionTypes.ADD_TO_CART:
       return [
         ...state,
         action.payload
         ];
+    case actionTypes.REMOVE_FROM_CART:
     default:
       return state;
   }
