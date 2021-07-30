d64eae4ebce6a201f721dabc6ffc02266ec7ec41
Michelle-Gn <michelle.guan93@gmail.com>
2021-07-29 18:51:19 -0700

    edit API_KEY

--- client/src/actions/Q&A/getQuestions.js
+++ client/src/actions/Q&A/getQuestions.js
@@ -1,13 +1,14 @@
 import * as actionTypes from '../actionTypes.js'
 import axios from 'axios'
+var API_KEY = require('../../../../config.js')
 
-const API_KEY = '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';
+// console.log('api_key', API_KEY.TOKEN)
 
 export default (dispatch, product_id) => {
 
   const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions?product_id=${product_id}`
   const options = {
-    headers: {'Authorization': API_KEY}
+    headers: {'Authorization': API_KEY.TOKEN}
   }
   axios.get(url, options).then(res => {
     dispatch({
