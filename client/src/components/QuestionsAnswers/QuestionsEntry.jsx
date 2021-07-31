2ac774ad672270ce2cefccff93fa5e3be272f820
Michelle-Gn <michelle.guan93@gmail.com>
2021-07-31 13:51:37 -0700

    implement answers rendering

--- client/src/components/QuestionsAnswers/QuestionsEntry.jsx
+++ client/src/components/QuestionsAnswers/QuestionsEntry.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
-import AnswerList from './AnswerList.jsx';
+import React, {useEffect, useState} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import getAnswers from '../../actions/Q&A/getAnswers.js';
 
-	var QuestionsEntry = (props) => (
-	  <div>
-	    <li> {props.question.question_body} </li>
-	      <span>Helpful? Yes ({props.question.question_helpfulness})</span>
-	      <span>Add Answer</span>
-	    <AnswerList question_id = {props.question.question_id}/>
-	  </div>
-	)
+import Answer from './Answer.jsx'
+
+	var QuestionsEntry = (props) => {
+    // make api call to get answer using props
+		const dispatch = useDispatch();
+		console.log(props.question.question_id);
+		const [answers, setAnswers] = useState([]);
+    useEffect(() => {getAnswers(props.question.question_id)
+			.then((result) => {
+			setAnswers (result.data.results);
+		  })
+			.catch((error) => {console.log(error)})}, []);
+
+		// const localState = useSelector(((globalState) => globalState.answers));
+
+		// slice answer list to get only 2 answers
+		const [count, setCount] = useState(2);
+	  let answerList = answers.slice(0, count);
+		console.log(answerList);
+
+		// map over each entry
+		// sort answer list
+    // var answerslist = useSelector()
+		if (answerList.length !== 0) {
+			return (
+			<div>
+				<div> {'Q: ' + props.question.question_body} </div>
+				<span className = "helpful">Helpful?</span>
+				<span className = "helpfulness-stats"> Yes ({props.question.question_helpfulness})</span>
+				<span>Add Answer</span>
+				<div>
+				{answerList.map((answer) => (
+					<Answer answer = {answer} key = {answer.answer_id}/>
+				))}
+				</div>
+			</div>
+			)
+		} else {
+			return null;
+		}
+	}
 
 	export default QuestionsEntry
