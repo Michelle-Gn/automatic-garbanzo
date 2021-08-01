db3dd9eba0ad8d6511c9bbb3812e6dbdcc0d5a73
Michelle-Gn <michelle.guan93@gmail.com>
2021-07-31 20:43:11 -0700

    implement get questions action creator

--- client/src/actions/Q&A/getQuestions.js
+++ client/src/actions/Q&A/getQuestions.js
@@ -2,15 +2,16 @@ import * as actionTypes from '../actionTypes.js'
 import axios from 'axios'
 var API_KEY = require('../../../../config.js')
 
-// console.log('api_key', API_KEY.TOKEN)
+//page =1
+// 1000 count
 
-export default (dispatch, product_id) => {
-
-  const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions?product_id=${product_id}`
+export default (dispatch, product_id, page, count) => {
+  const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions?product_id=${product_id}&page=${page}&count=${count}`
   const options = {
     headers: {'Authorization': API_KEY.TOKEN}
   }
-  axios.get(url, options).then(res => {
+  axios.get(url, options)
+  .then(res => {
     dispatch({
       type: actionTypes.GET_QUESTIONS,
       payload: res.data.results
