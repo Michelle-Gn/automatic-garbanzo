5b35f9f41f126ef09659efd965ddff9ee11e3189
Michelle-Gn <michelle.guan93@gmail.com>
2021-07-31 20:46:19 -0700

    update action types list with q&a actions

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -4,9 +4,10 @@
 // Used in Product Overview Reducers
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const UPDATE_SELECTED_STYLE = 'UPDATE_SELECTED_STYLE';
-export const UPDATE_STYLE_LIST = 'UPDATE_STYLE_LIST'
+export const UPDATE_STYLE_LIST = 'UPDATE_STYLE_LIST';
 // Used in Reviews Reducers
 export const GET_RATINGS = 'GET_RATINGS';
 export const SET_TOTAL_RATINGS_AND_AVERAGE = 'SET_TOTAL_RATINGS_AND_AVERAGE';
 // Used in Question Answer
 export const GET_QUESTIONS = 'GET_QUESTIONS';
+export const GET_ANSWERS = 'GET_ANSWERS';
