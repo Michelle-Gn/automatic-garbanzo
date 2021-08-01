82ffe18fd4b2c9eb0ce1fb3102dd9ab57361f282
Michelle-Gn <michelle.guan93@gmail.com>
2021-07-31 20:48:09 -0700

    implement questions component

--- client/src/components/QuestionsAnswers/Questions.jsx
+++ client/src/components/QuestionsAnswers/Questions.jsx
@@ -1,44 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import QuestionsEntry from './QuestionsEntry.jsx';
 import getQuestions from '../../actions/Q&A/getQuestions.js';
 
 var Questions = (props) => {
- // uses an action and dispatches it to the store to modify state
- // the action is passed through all the reducers in the store and the relevant
- // reducer uses the action to change state
-
-	const dispatch = useDispatch();
-// defines dispatch
-  getQuestions(dispatch, '16058');
-// invokes the getQuestions action, with dispatch as arg1, and product_id as arg 2
-	const questions = useSelector(state => state.questions)
-// useSelector gets pieces of data from global state
-
+if (props.qaList.length !== 0){
 	return (
 		<div>
-			<ul>
-				{questions.map((question, index) => (
-					<QuestionsEntry question = {question} key = {index}/>
+			<div>
+				{props.qaList.map((question) => (
+          <QuestionsEntry question = {question} key = {question.question_id}/>
 				))}
-			</ul>
+			</div>
 		</div>
 	)
+} else {
+	return null;
+}
 }
 
-
-	export default Questions
-
-
-	// getAnswer() {
-	// 	axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/questions?product_id=${this.state.product_id}`,
-	// 	{ headers: {'Authorization': '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'}})
-	// 	.then((results) => {
-	// 		this.setState ({
-	// 			allQuestions: results.data.results
-	// 		})})
-	// 	.catch((error) => {
-	// 		console.log(error)
-	// 	});
-	// }
+export default Questions
