ad55dae7b0f131d95bcecdf52e82dce1b8b2c772
Michelle-Gn <michelle.guan93@gmail.com>
2021-07-31 20:47:29 -0700

    implement quesions component

--- client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Questions from './Questions.jsx';
+import getQuestions from '../../actions/Q&A/getQuestions.js';
 
-	const QuestionsAnswers = () => {
+	const QuestionsAnswers = (props) => {
 
-	    return (
-	      <div>
-	        <h3> QUESTIONS AND ANSWERS </h3>
-	        <nav className="navbar">
-	          <div className="row">
-	            <div className="col-md-6 offset-md-3">
-	            <div className="search-bar form-inline">
-	                <input
-	                  className="form-control"
-	                  type="text"
-	                  placeholder= "HAVE A QUESTION? SEARCH FOR ANSWERS..."/>
-	                <button className="btn hidden-sm-down">
-	                  <span className="glyphicon glyphicon-search"></span>
-	                </button>
-	            </div>
-	            </div>
-	          </div>
-	        </nav>
-	        <div>
-	          <Questions/>
-	        <button className = "more-questions">
-	          MORE ANSWERED QUESTIONS
-	        </button>
-	        <button className = "add-question">
-	         ADD A QUESTION +
-	        </button>
-	        </div>
-	      </div>
-	    )
+    const dispatch = useDispatch();
+		useEffect(() => {getQuestions(dispatch, '16058', 1, 1000)}, []);
+		const localState = useSelector(((globalState) => globalState.questions));
+
+	  const [count, setCount] = useState(4);
+		let qaList = localState.slice(0, count);
+
+		if (qaList.length !== 0) {
+			return (
+				<div>
+					<h3> QUESTIONS AND ANSWERS </h3>
+					<nav className="navbar">
+						<div className="row">
+							<div className="col-md-6 offset-md-3">
+							<div className="search-bar form-inline">
+									<input
+										className="form-control"
+										type="text"
+										placeholder= "HAVE A QUESTION? SEARCH FOR ANSWERS..."/>
+									<button className="btn hidden-sm-down">
+										<span className="glyphicon glyphicon-search"></span>
+									</button>
+							</div>
+							</div>
+						</div>
+					</nav>
+					<div>
+						<Questions qaList = {qaList}/>
+					<button className = "more-questions">
+						MORE ANSWERED QUESTIONS
+					</button>
+					<button className = "add-question">
+					 ADD A QUESTION +
+					</button>
+					</div>
+				</div>
+			)
+		} else {
+			return null
+		}
 	}
 
 	export default QuestionsAnswers
