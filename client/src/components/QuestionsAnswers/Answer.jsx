088e26949a957fb63178d393de4d1d9bbe523789
Michelle-Gn <michelle.guan93@gmail.com>
2021-08-02 00:38:55 -0700

    implement answer component

--- client/src/components/QuestionsAnswers/Answer.jsx
+++ client/src/components/QuestionsAnswers/Answer.jsx
@@ -1,10 +1,17 @@
 import React, {useState} from 'react';
+import moment from 'moment';
+import AnswerHelpful from './AnswerHelpful.jsx';
+
 
 // one answer entry
   // format here
 var Answer = (props) => {
   return (
-    <div> {'A:' + props.answer.body}</div>
+  <div>
+    <div> {props.answer.body} </div>
+    <div>{props.answer.answerer_name + ", " + moment(props.answer.date).format("MMM Do YY")}</div>
+    <AnswerHelpful answer = {props.answer} />
+  </div>
   )
 }
 
