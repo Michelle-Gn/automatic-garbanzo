50919825c10dd82fe0eeafcf279bae0cf9102b63
Michelle-Gn <michelle.guan93@gmail.com>
2021-08-02 00:45:06 -0700

    pull out search bar into separate component

---
+++ client/src/components/QuestionsAnswers/SearchBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const SearchBar = (props) => {
+
+  return (
+  <div>
+  <h3> QUESTIONS AND ANSWERS </h3>
+    <nav className="navbar">
+      <div className="row">
+        <div className="col-md-50 offset-md-3">
+        <div className="search-bar form-inline">
+            <input
+              className="form-control"
+              type="text"
+              placeholder= "HAVE A QUESTION? SEARCH FOR ANSWERS..."/>
+            <button className="btn hidden-sm-down">
+              <span className="glyphicon glyphicon-search"></span>
+            </button>
+        </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  )
+}
+
+export default SearchBar;
