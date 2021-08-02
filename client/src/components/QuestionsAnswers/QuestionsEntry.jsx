03d3728fd6912fb43fef7fb7575c276e89900614
Michelle-Gn <michelle.guan93@gmail.com>
2021-08-01 22:03:08 -0700

    add add answer button and modal form component

--- client/src/components/QuestionsAnswers/QuestionsEntry.jsx
+++ client/src/components/QuestionsAnswers/QuestionsEntry.jsx
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Button } from 'react-bootstrap';
 import getAnswers from '../../actions/Q&A/getAnswers.js';
+import toggleShow from '../../actions/Q&A/toggleShow.js';
 
 import Answer from './Answer.jsx';
 import Helpful from './Helpful.jsx';
-import AddAnswer from './AddAnswer.jsx';
+import AnswerForm from './AnswerForm.jsx';
 
 var QuestionsEntry = (props) => {
-    // make api call to get answer using props
-		console.log(props.question.question_id);
+
 		const [answers, setAnswers] = useState([]);
     useEffect(() => {getAnswers(props.question.question_id)
 			.then((result) => {
@@ -20,19 +21,23 @@ var QuestionsEntry = (props) => {
 		const [count, setCount] = useState(2);
 	  let answerList = answers.slice(0, count);
 
-		// question helpfulness
-		 // update
 		const [helpfulness, setHelpfulness] = useState(props.question.question_helpfulness)
 
-		// map over each entry
 		// sort answer list
-    // var answerslist = useSelector()
+
+	  // create variable and set it equal to global piece of state for shown
+		const dispatch = useDispatch();
+		useEffect(() => {toggleShow(dispatch, true)}, []);
+
+		const localState = useSelector(((globalState) => globalState.answerFormStatus));
+
 		if (answerList.length !== 0) {
 			return (
 			<div>
 				<div> {'Q: ' + props.question.question_body} </div>
 				<Helpful question = {props.question} />
-				<AddAnswer questionId = {props.question.question_id} />
+				<Button onClick = {()=> {toggleShow(dispatch, localState)}}>Add Answer</Button>
+			  <AnswerForm show = {localState} question_id = {props.question.question_id}/>
 				<div>
 				{answerList.map((answer) => (
 					<Answer answer = {answer} key = {answer.answer_id}/>
