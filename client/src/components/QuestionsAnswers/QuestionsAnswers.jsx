d9c92904fd0bc7108b0d69e3747dbb7ebcba7f53
Michelle-Gn <michelle.guan93@gmail.com>
2021-08-03 11:15:33 -0700

    Update state in questions answers pass pass to search bar

--- client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -6,18 +6,26 @@ import getQuestions from '../../actions/Q&A/getQuestions.js';
 
 const QuestionsAnswers = (props) => {
 
+  const product = useSelector(state => state.getNewProductReducer.selectedProduct)
+	console.log('product_id', product.id)
+
 	const dispatch = useDispatch();
-	useEffect(() => {getQuestions(dispatch, '16058', 1, 1000)}, []);
+	useEffect(() => {getQuestions(dispatch, product.id, 1, 1000)}, [product]);
 	const localState = useSelector(((globalState) => globalState.questions));
 	const sortedLocalState = localState.sort((a, b) => b.question_helpfulness - a.question_helpfulness);
 
 	const [count, setCount] = useState(4);
 	let qaList = sortedLocalState.slice(0, count);
 
+	const [search, setSearch] = useState('');
+	if (search.length > 3) {
+		let filteredList = sortedLocalState.filter(sortedLocalState.question_body.includes(search))
+	}
+
 	if (qaList.length !== 0) {
 		return (
 			<div>
-				<SearchBar/>
+				<SearchBar searchField = {search} setSearch = {setSearch}/>
 				<div>
 				<Questions qaList = {qaList}/>
 				{count < localState.length &&
