ccdc9d180a27fed5b5c44b28d84f26d9aa563612
Michelle-Gn <michelle.guan93@gmail.com>
2021-08-03 11:04:45 -0700

    Add product review action type

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -5,6 +5,7 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const UPDATE_SELECTED_STYLE = 'UPDATE_SELECTED_STYLE';
 export const UPDATE_STYLE_LIST = 'UPDATE_STYLE_LIST';
+export const GET_NEW_PRODUCT = 'GET_NEW_PRODUCT';
 // Used in Reviews Reducers
 export const GET_RATINGS = 'GET_RATINGS';
 export const SET_TOTAL_RATINGS_AND_AVERAGE = 'SET_TOTAL_RATINGS_AND_AVERAGE';
