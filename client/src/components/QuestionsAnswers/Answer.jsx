2bd326a4b0911dc5dce3492093ae19a6418c8a86
Michelle-Gn <michelle.guan93@gmail.com>
2021-08-03 23:08:47 -0700

    Outline report answer function

--- client/src/components/QuestionsAnswers/Answer.jsx
+++ client/src/components/QuestionsAnswers/Answer.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import moment from 'moment';
 import AnswerHelpful from './AnswerHelpful.jsx';
+import AnswerReport from './AnswerReport.jsx';
 
 var Answer = (props) => {
   return (
@@ -8,6 +9,7 @@ var Answer = (props) => {
     <div> {props.answer.body} </div>
     <div>{props.answer.answerer_name + ", " + moment(props.answer.date).format("MMM Do YY")}</div>
     <AnswerHelpful answer = {props.answer} />
+    <AnswerReport answer_id = {props.answer.answer_id} />
   </div>
   )
 }
